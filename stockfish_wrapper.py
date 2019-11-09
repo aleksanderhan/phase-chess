6a50d81d2536d8fbfc4a6eef567711a7e3b8c1e1
Aleksander Hansen <aleksanderhan@gmail.com>
2019-11-09 18:00:31 +0100

    Removed file.

--- stockfish_wrapper.py
+++
@@ -1,8 +0,0 @@
-
-from stockfish import Stockfish
-
-
-class StockfishAPI(Stockfish):
-
-	def __init__(self, *args, **kwargs):
-		super().__init__(self, path: str = "stockfish", depth: int = 2, params: dict = None)
